511529e64e14cbd3a4ed2105b69edbae089956fe
Aleff Carmona <aleff.carmonaferreira@gmail.com>
2026-08-28 16:19:27 -0300

    Fix return for get realms function

--- main.py
+++ main.py
@@ -150,10 +150,8 @@ async def get_server_realms(db: AsyncSession = Depends(get_db), user: dict = Dep
 
     realms_result = (await db.execute(realms_stmt)).all()
 
-    realm_parse = { 'name': name, 'string_id': string_id for name, string_id in realms_result}
-
     return {
-        'data': realm_parse
+        'data': [{'name': name, 'string_id': string_id} for name, string_id in realms_result]
     }
 
 @app.get("/server_analytics")
